f8b2da96d107c38c0c956519c31b8a87c8c2aec0
Gabriele Lippi <gabriele@lippi.net>
2019-03-01 12:33:00 +0100

    Refactor production code

--- src/main/java/StringCalculatorProvider.java
+++ src/main/java/StringCalculatorProvider.java
@@ -1,13 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class StringCalculatorProvider {
 
   public int Add(String numbers) {
-      if (numbers.length() > 1) return parseStringToNumber(numbers);
+      if (numbers.length() > 1) return parseStringsToNumber(numbers);
       if (numbers != "") return Integer.parseInt(numbers);
       return 0;
   }
 
-  private int parseStringToNumber(String numbers) {
-      return Integer.parseInt(numbers.split(",")[0]) + Integer.parseInt(numbers.split(",")[1]);
+  private int parseStringsToNumber(String numbers) {
+      String[] splittedString = splitOnComma(numbers);
+      return Integer.parseInt(splittedString[0]) + Integer.parseInt(splittedString[1]);
+  }
+
+  private String[] splitOnComma(String numbers) {
+      return numbers.split(",");
   }
 
 }
